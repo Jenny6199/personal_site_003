28a57cdf8bce2807a12b668cf869190fc2e6a8fb
Maksim <Jenny6199@yandex.ru>
2023-06-18 16:15:04 +0300

    create mainapp.urls.py

---
+++ core/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from .views import index, categories, archive
+
+urlpatterns = [
+    path('', index),
+    path('categories/<slug:cat_id>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+]
--- core/mainapp/views.py
+++ core/mainapp/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Главная страница сайта</h1>")
+
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Страница категорий</h1><p>{cat_id}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
