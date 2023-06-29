4ce56dd8bacb049cbe61b081288a7b1cf8027ba9
Maksim_Sapunov <Jenny6199@yandex.ru>
2023-06-29 14:51:32 +0300

    'models_autor_article_edit'

--- core/mainapp/admin.py
+++ core/mainapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Author, Article
 
-# Register your models here.
+
+@admin.register(Author)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = [
+        'surname',
+        'name',
+        'parent_name',
+        'article_counter',
+        'speciality',
+        'registered_at',
+    ]
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'category',
+        'author',
+        'image',
+        'created_time',
+        'updated_time',
+        'visitors_counter',
+    ]
--- core/mainapp/models.py
+++ core/mainapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """
+    Model for author for site's mainapp
+    """
+    # Fields
+    name = models.CharField(max_length=62, verbose_name='имя')
+    surname = models.CharField(max_length=62, verbose_name='фамилия')
+    parent_name = models.CharField(max_length=62, verbose_name='отчество', blank=True)
+    article_counter = models.PositiveIntegerField(default=0, verbose_name='количество статей')
+    speciality = models.CharField(max_length=128, verbose_name='специальность')
+    registered_at = models.DateTimeField(auto_now_add=True, verbose_name='дата регистрации')
+
+    # Metadata
+    class Meta:
+        ordering = ['surname']
+        verbose_name = 'автор'
+        verbose_name_plural = 'авторы'
+
+    # Methods
+    def __str__(self):
+        return f'{self.surname} {self.name}'
+
+
+class Article(models.Model):
+    """
+    Model for article for site's mainapp
+    """
+    # Fields
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    category = models.CharField(max_length=128, blank=False, verbose_name='категория')
+    author = models.OneToOneField(Author, on_delete=models.PROTECT, primary_key=True, verbose_name='автор')
+    image = models.ImageField(blank=True, upload_to='images/', verbose_name='изображение')
+    text = models.TextField(default='', verbose_name='текст статьи', blank=False)
+    created_time = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='дата сознания')
+    updated_time = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+    visitors_counter = models.PositiveIntegerField(verbose_name='количество просмотров')
+
+    # Metadata
+    class Meta:
+        ordering = ['category']
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
+
+    # Methods
+    def __str__(self):
+        return self.title
