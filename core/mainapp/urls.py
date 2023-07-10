214d807483622456bac1b3428b6dfa1b33ead622
Maksim_Sapunov <Jenny6199@yandex.ru>
2023-07-10 12:11:50 +0300

    add_contacts_page

--- core/mainapp/urls.py
+++ core/mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, contacts
 
 urlpatterns = [
     path(route="", view=index, name='index', kwargs={}),
+    path(route="contacts", view=contacts, name='contacts', kwargs={}),
 ]
--- core/mainapp/views.py
+++ core/mainapp/views.py
@@ -5,8 +5,15 @@ from .models import Author, Article
 def index(request):
     articles = Article.objects.all()
     context = {
-        'title': 'Главная',
+        'title': 'главная',
         'menu': None,
         'articles': articles,
     }
     return render(request, 'mainapp/index.html', context=context)
+
+
+def contacts(request):
+    context = {
+        'title': 'контакты',
+    }
+    return render(request, 'mainapp/contacts.html', context=context)
