7a7f5be0a3d98a529553663bcef2303d59acca1f
Maksim <Jenny6199@yandex.ru>
2023-08-06 17:56:18 +0300

    change_number_of_fields_in_AddArticleForm

--- core/mainapp/forms.py
+++ core/mainapp/forms.py
@@ -5,4 +5,10 @@ from .models import Article
 class AddArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = [
+            'title',
+            'category',
+            'author',
+            'image',
+            'text'
+        ]
