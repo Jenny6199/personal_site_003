cb23d2b3674123aa1b541fec5fbd35ebcb2e64db
Maksim_Sapunov <Jenny6199@yandex.ru>
2023-08-30 16:27:09 +0300

    start_way_to_CBV

--- core/mainapp/urls.py
+++ core/mainapp/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import index, contacts, articles, articles_categories, news, show_article, add_article
 
 urlpatterns = [
+    # Mainpage
     path(route="", view=index, name='index', kwargs={}),
+    # Articles
     path(route="articles/", view=articles, name='articles', kwargs={}),
     path(route="articles/categories/<slug:cat_slug>/", view=articles_categories, name='articles_categories', kwargs={}),
-    path(route="news/", view=news, name='news', kwargs={}),
-    path(route="contacts/", view=contacts, name='contacts', kwargs={}),
     path(route="article/<slug:article_slug>", view=show_article, name='show_article', kwargs={}),
     path(route="add_article/", view=add_article, name='add_article', kwargs={})
+    # News
+    path(route="news/", view=news, name='news', kwargs={}),
+    # Contacts
+    path(route="contacts/", view=contacts, name='contacts', kwargs={}),
 ]
